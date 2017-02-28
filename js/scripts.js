ab729563cadab68765fe93cf0f0fb7a1f05770e8
Matt Brown <matt388@gmail.com>
2017-02-28 00:28:05 -0800

    scrolling to sections done. All possible links working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,35 @@
+var emptyLink = document.querySelectorAll("a[href='#']");
+
 $(document).ready(function(){
 	$('#slideshow').carousel({
   				interval: 0
 		});
 });
+
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    $('#nav').toggleClass('darknav',
+     //add 'darknav' class when div position match or exceeds else remove the 'ok' class.
+      scroll >= $('#about').offset().top - 50
+    );
+});
+//trigger the scroll
+$(window).scroll();
+
+//Scroll to section
+$("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top - 35
+    });
+});
+
+$(emptyLink).click(function(e) {
+    e.preventDefault();
+
+});
+
+
 
